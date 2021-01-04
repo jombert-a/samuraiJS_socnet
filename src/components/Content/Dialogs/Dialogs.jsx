c755b715287a1755a0f9e3ccd487dd86e03a4bb3
rbrta <nonrobertsbox@gmail.com>
2021-01-04 05:44:44 +0500

    addmessage v.1 (alert message)

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -1,9 +1,16 @@
 import Dialog from './Dialog/Dialog'
 import s from './Dialogs.module.css'
 import Messages from './Messages/Messages'
+import React from 'react';
 const Dialogs = (props) => {
     let dialogsArray = props.data.dialogsData.map(el => <Dialog name={el.name} id={el.id} avaSrc={el.avaSrc} />);
-    let messagesArray = props.data.messagesData.map(el => <Messages data={el}/>);
+    let messagesArray = props.data.messagesData.map(el => <Messages data={el} />);
+    let newMessageArea = React.createRef();
+
+    let addMessage = () => {
+        let text = newMessageArea.current.value;
+        alert(text);
+    }
     return (
         <div className={s.dialogsWrapper}>
             <div className={s.dialogs}>
@@ -11,6 +18,10 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesArray}
+                <div className={s.addmessage}>
+                    <textarea ref={newMessageArea}></textarea>
+                    <button onClick={addMessage}>Send message</button>
+                </div>
             </div>
         </div>
     );
